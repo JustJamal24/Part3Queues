fdeba735d8d32ebeb20830a6df3e7a11813a6f52
jeremiahalajemba <JeremiahAlajemba@gmail.com>
2024-04-15 14:56:05 +0100

    Created a class called task and added the required fields and the parameterised constructor

--- src/main/java/org/example/Task.java
+++ src/main/java/org/example/Task.java
@@ -1,2 +1,33 @@
-package org.example;public class Task {
+package org.example;
+
+import java.time.LocalDate;
+
+public class Task {
+
+    // Fields
+    private String owner;
+    private String description;
+
+    private LocalDate deadline;
+
+    // Constructor
+    public Task(String owner, String description, LocalDate deadline) {
+        this.owner = owner;
+        this.description = description;
+        this.deadline = deadline;
+        LocalDate today = LocalDate.now();
+        // Check if deadline is before or today's date'
+        // If deadline is before or today's date, then throw an IllegalArgumentException'
+        if (deadline.compareTo(today) <= 0 || deadline.compareTo(today) == 0) {
+            throw new IllegalArgumentException("Deadline cannot be before or equal to today's date");
+        }
+    }
+
+
+
+
+
+
+
+
 }
