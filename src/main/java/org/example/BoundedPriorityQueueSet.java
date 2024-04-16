1459cef7785e753d55eaf508904f1704d1213d16
jeremiahalajemba <JeremiahAlajemba@gmail.com>
2024-04-16 07:20:14 +0100

    Extended the LinkedList class and created a default constructor to initialise the BoundedPriorityQueueSet with a maximum size of 10 and created a size method to return the number of elements in the queue

--- src/main/java/org/example/BoundedPriorityQueueSet.java
+++ src/main/java/org/example/BoundedPriorityQueueSet.java
@@ -1,6 +1,16 @@
 package org.example;
 
 public class BoundedPriorityQueueSet  extends LinkedList {
+    public BoundedPriorityQueueSet() {
+        super(10);
+
+    }
+    public BoundedPriorityQueueSet(int maxSize) {
+        super(maxSize);
+
+    }
+
+    public int size() {return super.size();}
 
 
 
