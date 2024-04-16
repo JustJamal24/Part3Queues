4c4249f28e9aa744ce6f51465dd34e9724578ee1
jeremiahalajemba <JeremiahAlajemba@gmail.com>
2024-04-16 05:31:26 +0100

    Defined the Node class and the getters and setters added the fields for the LinkedList and created the default constructor. And set the maximum size of the list to 10.

--- src/main/java/org/example/LinkedList.java
+++ src/main/java/org/example/LinkedList.java
@@ -5,6 +5,66 @@ import java.util.NoSuchElementException;
 
 public class LinkedList {
 
+    // Fields
+    private Node head;
+    private Node tail;
+    private int numberOfTasks;
+    private final int maxSize;
+
+    // default constructor
+    public LinkedList() {
+        this.head = null;
+        this.tail = null;
+        this.numberOfTasks = 0;
+        this.maxSize = 10;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    protected static class Node {
+        // Fields
+        private Task data;
+        private Node next;
+
+        public Node(Task data) {
+            this.data = data;
+            this.next = null;
+        }
+
+        //Setters and Getters
+        
+        public void setNext(Node next) {
+            this.next = next;
+        }
+
+        public Node getNext() {
+            return this.next;
+        }
+
+        public void setData(Task data) {
+            this.data = data;
+        }
+
+        public Task getData() {
+            return this.data;
+        }
+    }
+
 
 }
 
