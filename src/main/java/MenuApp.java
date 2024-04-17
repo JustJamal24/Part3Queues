71369dbcb7758063af1abfdc6e3a39873eea1aef
sophie <d00259237@student.dkit.ie>
2024-04-17 15:52:07 +0100

    Added risky and maxSize variables

--- src/main/java/MenuApp.java
+++ src/main/java/MenuApp.java
@@ -7,6 +7,11 @@ public class MenuApp {
       Scanner sc = new Scanner(System.in);
       Random rg = new Random();
 
+      boolean risky = rg.nextBoolean();
+
+        System.out.println("Hello, Please enter what size you would like your queue to be: ");
+        int maxSize = sc.nextInt();
+
 
 
 
