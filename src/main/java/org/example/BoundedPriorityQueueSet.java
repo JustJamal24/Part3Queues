ac7270911d046cf513f90959ea453708e6e2e97d
sophie <d00259237@student.dkit.ie>
2024-04-17 15:06:16 +0100

    Added isEmpty(), isFull(), and add methods called from LinkedList

--- src/main/java/org/example/BoundedPriorityQueueSet.java
+++ src/main/java/org/example/BoundedPriorityQueueSet.java
@@ -12,15 +12,24 @@ public class BoundedPriorityQueueSet  extends LinkedList {
 
     public int size() {return super.size();}
 
-    public boolean isEmpty(){
-        return super.isEmpty();
-    }
 
-
+    public boolean isEmpty() {return super.isEmpty();}
 
     public boolean isFull(){
         return super.isFull();
     }
+
+    public void add(Task task){
+        if(isFull()){
+            //if the queue is full, an exception is thrown
+            throw new IllegalStateException("Queue is full");
+        }
+        super.add(task);
+    }
+
+
+
+
 
 
 
